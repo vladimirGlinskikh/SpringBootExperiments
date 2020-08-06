4ca00ed0e01d36e25515a1a74f556e6229136b98
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-06 22:51:03 +0600

    Experiments with mustache

--- src/main/java/kz/zhelezyaka/sweater/GreetingController.java
+++ src/main/java/kz/zhelezyaka/sweater/GreetingController.java
@@ -5,11 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Java") String name, Map<String, Object> model) {
+        model.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "It's, nice");
+        return "main";
     }
 }
